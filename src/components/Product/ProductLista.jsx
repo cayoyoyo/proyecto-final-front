706a9335a81557d345ac87b9a51c2b092c57fdb4
joao carlos <joaocarlosnnx@gmail.com>
2023-09-30 14:35:26 +0200

    despues del merge

---
+++ src/components/Product/ProductLista.jsx
@@ -0,0 +1,49 @@
+
+
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import FavoritosButton from "../FavoriteButton";
+
+function ProductList() {
+    const API_URL = "http://localhost:5005";
+
+    const [products, setProducts] = useState([]);
+
+    // Cargar la lista de productos y su estado de favoritos al cargar la página
+    useEffect(() => {
+        axios
+            .get(`${API_URL}/product`)
+            .then((response) => {
+                setProducts(response.data);
+            })
+            .catch((err) => console.log("Error al cargar la lista de productos: " + err));
+    }, []);
+
+    return (
+        <div>
+            <h1>Lista de Productos</h1>
+            <ul className="ulListaProdutos">
+                {products.map((producto) => (
+                    <li className="liListaProdutos" key={producto._id}>
+                        <FavoritosButton id={producto._id} favorito={producto.isFavorite} />
+
+                        <img
+                            className="product-image"
+                            src={producto.images[0]}
+                            alt={producto.title}
+                        />
+                        <p className="titleProducto">{producto.title}</p>
+                        <p>€ {producto.price}</p>
+
+                        <Link to={`/product/${producto._id}`}>
+                            <button className="btnProducto">Ver más </button>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default ProductList;
